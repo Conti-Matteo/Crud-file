11bfb21477b7769220d56d78afd3588d5e2c1295
Matteo Conti <matconti06@gmail.com>
2023-04-14 13:36:03 +0200

    Aggiungere i file di progetto.

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Aggiungi un prodotto al carrello: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Inserisci il prezzo del prodotto: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(177, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Crea ed aggiorna il file con il prodotto inserito";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(571, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(217, 350);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(568, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Carrello:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(231, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Togli o modifica un prodotto inserito nel carrello:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Togli un prodotto dal file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 35);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Ricerca un prodotto nel file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Inserisci un prodotto da ricercare: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(174, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Nuovo prezzo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Nuovo prodotto:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(177, 179);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(141, 20);
+            this.textBox5.TabIndex = 25;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 179);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(141, 20);
+            this.textBox4.TabIndex = 24;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(324, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 35);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Togli il prodotto ricercato e sostituiscilo dal file\r\n\r\n";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(689, 381);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 57);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "Stampa il file nel carrello";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(571, 381);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 57);
+            this.button6.TabIndex = 29;
+            this.button6.Text = "Cancella il file";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(324, 140);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(153, 23);
+            this.button7.TabIndex = 30;
+            this.button7.Text = "Ripristina i prodotti cancellati";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CRUD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ CRUD/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        #region dichiarazione e inizializzazione variabili globali
+        string filename;
+
+        public Form1()
+        {
+            InitializeComponent();
+            filename = @"carrello.csv";
+        }
+        #endregion
+
+        #region eventi
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //richiamo la funzione di creazione e aggiornamento del file
+            aggiornamentofile(textBox1.Text, textBox2.Text);
+
+            //pulisco le textBox per un nuovo inserimento
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+            //seleziono in automatico la prima textBox
+            textBox1.Select();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //richiamo la funzione di ricerca "falsa"
+            falsesearch(textBox3.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //controllo cancellazione logica
+            cancl(textBox3.Text);
+
+            //pulisco le textBox per un nuovo inserimento
+            textBox3.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ripr();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //richiamo la funzione di modifica
+            mod(textBox3.Text, textBox4.Text, textBox5.Text);
+
+            //pulisco le textBox per un nuovo inserimento
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            visualizza();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            delete();
+        }
+
+        #endregion
+
+        #region funzioni di servizio
+        //funzione di creazione e aggiornamento file
+        void aggiornamentofile(string nome, string prezzo)
+        {
+            //verifico che non ci siano elementi nello struct per creare il file
+            if (!File.Exists(filename))
+            {
+                //creazione del file
+                using (StreamWriter sw = new StreamWriter(filename, append: false))
+                {
+                    //copio nel file le stringhe delle textBox
+                    sw.WriteLine(nome + " €" + prezzo + "; true");
+
+                    //chiudo il file
+                    sw.Close();
+                }
+            }
+
+            //nel caso sia già stato creato il file, lo aggiorno
+            else
+            {
+                //apro il file
+                using(StreamWriter sw = new StreamWriter(filename, append: true))
+                {
+                    //copio nel file le stringhe delle textBox
+                    sw.WriteLine(nome + " €" + prezzo + "; true");
+
+                    //chiudo il file
+                    sw.Close();
+                }
+            }
+
+        }
+
+        //funzione di ricerca "falsa"
+        void falsesearch(string nome)
+        {
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                //creo una stringa momentanea
+                string s;
+
+                //mentre la stringa momentanea non diventa nulla assumeno i valori delle stringhe nel file...
+                while((s = sr.ReadLine()) != null)
+                {
+                    //... se il nome appartiene alla stringa ...
+                    if (s.Contains(nome))
+                    {
+                        //... stampo una messagebox di ritrovamento...
+                        MessageBox.Show("La stringa è stata trovata");
+
+                        //... e ritorno al programma ...
+                        return;
+                    }
+                }
+
+                //... altrimenti ...
+                {
+                    //... stampo una messagebox che avvisa che la stringa non esiste...
+                    MessageBox.Show("La stringa non è stata trovata");
+                }
+
+                //chiudo il file
+                sr.Close();
+            }
+        }
+
+        //funzione di controllo cancellazione logica
+        void cancl(string nome)
+        {
+            //stringa momentanea
+            string s;
+
+            int i = 0;
+
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                while ((s = sr.ReadLine()) != null)
+                {
+                    i++;
+                }
+
+                //chiudo il file
+                sr.Close();
+
+                string[] name = new string[i];
+                using (StreamWriter sr2 = new StreamWriter(filename))
+                {
+                    for (int j = 0; j < name.Length; j++)
+                    {
+                        if (s.Contains(nome))
+                        {
+                            sr2.WriteLine(s + "; true");
+                        }
+                        else
+                        {
+                            sr2.WriteLine(s + "; false");
+                        }
+                    }
+                }
+            }
+        }
+
+        void ripr()
+        {
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                //stringa momentanea
+                string s;
+
+                using (StreamWriter sr2 = new StreamWriter(filename, append: false))
+                {
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        if (!s.Contains("true"))
+                        {
+                            //divido la stringa in varie sottostringhe
+                            string[] words = s.Split(' ');
+
+                            //ciclo di stampa e controllo
+                            for (int i = 0; i < words.Length; i++)
+                            {
+                                if (words[i] != "false" && i + 1 != words.Length)
+                                {
+                                    sr2.Write(words[i] + " ");
+                                }
+                                else
+                                {
+                                    sr2.WriteLine("; true");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            sr2.WriteLine(s);
+                        }
+                    }
+                }
+            }
+        }
+
+        //funzione di modifica
+        void mod(string ricerca, string nome, string prezzo)
+        {
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                //creo una stringa momentanea
+                string s;
+
+                //creo un file temporaneo
+                using (StreamWriter sr2 = new StreamWriter("temp.csv", append: true))
+                {
+                    //mentre la stringa momentanea non diventa nulla assumeno i valori delle stringhe nel file...
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        //... se nella stringa momentanea non è presente il nome ricercatoe ...
+                        if (s.Contains(ricerca))
+                        {
+                            //... stampo la stringa modificata nel file temporaneo ...
+                            sr2.WriteLine(nome + " €" + prezzo + "; true");
+                        }
+                        //... mentre se la stringa dovesse corrispondere ...
+                        else
+                        {
+                            //... la stampo nel file temporaneo ...
+                            sr2.WriteLine(s);
+                        }
+                    }
+                }
+            }
+            //cancello il file principale
+            File.Delete(filename);
+
+            //e rinomino il file momentaneo, rendendolo il nuovo principale
+            File.Move("temp.csv", filename);
+        }
+
+        //funzione di stampa del file
+        void visualizza ()
+        {
+            //pulisco la listview per stampare il file
+            listView1.Items.Clear();
+
+            //verifico che il file esista
+            if(!File.Exists(filename))
+            {
+                MessageBox.Show("Il file non è ancora stato creato");
+            }
+            else
+            {
+                using (StreamReader sr = File.OpenText(filename))
+                {
+                    //creo una stringa momentanea
+                    string s;
+
+                    //mentre la stringa momentanea non diventa nulla assumeno i valori delle stringhe nel file...
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        if (s.Contains("true"))
+                        {
+                            //... e stampo la stringa nella listview
+                            listView1.Items.Add(s);
+                        }
+                    }
+
+                    //chiudo il file
+                    sr.Close();
+                }
+            }
+        }
+
+        //funzione di cancellamento del file
+        void delete()
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+        }
+        #endregion
+    }
+}
